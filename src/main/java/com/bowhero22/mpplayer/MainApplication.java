06e977cb54267eb2ee78db0fbafbe4191f41b377
bowhero22 <bowhero22@gmail.com>
2022-07-17 17:00:26 +0900

    MPPlayer Beta

--- src/main/java/com/bowhero22/mpplayer/MainApplication.java
+++ src/main/java/com/bowhero22/mpplayer/MainApplication.java
@@ -3,7 +3,9 @@ package com.bowhero22.mpplayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -13,8 +15,9 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainLayout.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PlayerLayout.fxml"));
         scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setTitle("MPPlayer");
         stage.setScene(scene);
         stage.show();
