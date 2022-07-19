bd116120b3702148bc340a0f9db3328a1aeea592
bowhero22 <bowhero22@gmail.com>
2022-07-19 13:27:45 +0900

    MPPlayer Beta

--- src/main/java/com/bowhero22/mpplayer/controllers/PlayerLayoutController.java
+++ src/main/java/com/bowhero22/mpplayer/controllers/PlayerLayoutController.java
@@ -2,63 +2,16 @@ package com.bowhero22.mpplayer.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PlayerLayoutController implements Initializable {
     @FXML
-    private Label artistInfo_lbl;
+    private TreeView<String> libraryTreeView;
 
-    @FXML
-    private Label album_lbl;
-
-    @FXML
-    private Label fileFormat_lbl;
-
-    @FXML
-    private Label albumInfo_lbl;
-
-    @FXML
-    private Label track_lbl;
-
-    @FXML
-    private Label year_lbl;
-
-    @FXML
-    private Label genreInfo_lbl;
-
-    @FXML
-    private Label genre_lbl;
-
-    @FXML
-    private Label trackInfo_lbl;
-
-    @FXML
-    private Label artist_lbl;
-
-    @FXML
-    private Label titleInfo_lbl;
-
-    @FXML
-    private Label fileFormatInfo_lbl;
-
-    @FXML
-    private Label comment_lbl;
-
-    @FXML
-    private Label commentInfo_lbl;
-
-    @FXML
-    private Label title_lbl;
-
-    @FXML
-    private ProgressBar musicProgresszBar;
-
-    @FXML
-    private Label yearInfo_lbl;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
