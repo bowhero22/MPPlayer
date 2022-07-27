61f06f216852dac99fe1242220e55b8005978544
bowhero22 <bowhero22@gmail.com>
2022-07-27 20:27:58 +0900

    MPPlayer Beta

--- src/main/java/com/bowhero22/mpplayer/controllers/PlayerLayoutController.java
+++ src/main/java/com/bowhero22/mpplayer/controllers/PlayerLayoutController.java
@@ -5,12 +5,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class PlayerLayoutController implements Initializable {
 
@@ -34,31 +37,52 @@ public class PlayerLayoutController implements Initializable {
 
     public static ImageView[] artistImageView;
 
-    public static  ImageView[] genreImageView;
+    public static ImageView[] genreImageView;
+
+    public static ImageView[] generalImageView;
+
+    public static TreeItem<String> artistRoot;
+    public static TreeItem<String> albumRoot;
+    public static TreeItem<String> genreRoot;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        artistImageView = createNewImageView(50, 50, 512);
-        albumImageView = createNewImageView(50, 50, 512);
-        genreImageView = createNewImageView(50, 50, 512);
+        //TO-DO: Create new icon and apply icon to tree view.
+        artistImageView = createNewImageView(50, 50, 512, null);
+        albumImageView = createNewImageView(50, 50, 512, null);
+        genreImageView = createNewImageView(50, 50, 512, null);
+        generalImageView = createNewImageView(50,50,512,null);
 
-        TreeItem<String> artistRoot = new TreeItem<>("Artist", albumImageView[0]);
-        TreeItem<String> albumRoot = new TreeItem<>("Album", artistImageView[0]);
-        TreeItem<String> genreRoot = new TreeItem<>("Genre", genreImageView[0]);
+        artistRoot = new TreeItem<>("Artist", albumImageView[0]);
+        albumRoot = new TreeItem<>("Album", artistImageView[0]);
+        genreRoot = new TreeItem<>("Genre", genreImageView[0]);
 
         artistTreeView.setRoot(artistRoot);
         albumTreeView.setRoot(albumRoot);
         genreTreeView.setRoot(genreRoot);
 
+        TreeItem<String> noItem = new TreeItem<>("No Item", generalImageView[0]);
+
+        //TreeView initialization with "No Item" item.
+
+        artistRoot.getChildren().add(noItem);
+        albumRoot.getChildren().add(noItem);
+        genreRoot.getChildren().add(noItem);
     }
 
-    private ImageView[] createNewImageView(int height, int width, int size) {
-        ImageView[] imageView = new ImageView[size];
+    private ImageView[] createNewImageView(int height, int width, int arrsize, Image icon) {
+        Boolean noIcon = icon == null;
+
+        ImageView[] imageView = new ImageView[arrsize];
         for(ImageView i : imageView) {
-            i = new ImageView();
-            i.setFitHeight(50);
-            i.setFitWidth(50);
+            if(noIcon) {
+                i = new ImageView();
+            } else {
+                i = new ImageView(icon);
+            }
+            i.setFitHeight(height);
+            i.setFitWidth(width);
         }
 
         return imageView;
